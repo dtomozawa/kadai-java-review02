32cd76efa8d7674de05e9b858472057904e78b1e
dtomozawa <daisuke.tomozawa@gmail.com>
2023-01-29 19:04:57 +0900

    aをiに修正

--- src/Review02.java
+++ src/Review02.java
@@ -3,15 +3,15 @@ public class Review02 {
 
     public static void main(String[] args) {
         
-            for(int a = 1; a <= 100; a++) {
-                if(a % 15 == 0) {
+            for(int i = 1; i <= 100; i++) {
+                if(i % 15 == 0) {
                     System.out.println("FizzBuzz");
-                } else if(a % 3 == 0) {
+                } else if(i % 3 == 0) {
                     System.out.println("Fizz");
-                } else if(a % 5 == 0) {
+                } else if(i % 5 == 0) {
                     System.out.println("Buzz");
                 } else {
-                    System.out.println(a);
+                    System.out.println(i);
                 }
             }
         }
